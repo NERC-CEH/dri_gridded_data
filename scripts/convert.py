a3cacc025110cca4721d6ab44be1dc5349d4197c
mattjbr123 <mattjbr123@gmail.com>
2026-05-15 18:15:46 +0100

    Restore deleted essential line

--- scripts/convert.py
+++ scripts/convert.py
@@ -2,6 +2,7 @@ import argparse
 from dri_gridded_data.converters import converter
 from dri_gridded_data.utils import load_yaml_config
 
+parser = argparse.ArgumentParser()
 parser.add_argument("configpath", type=str, help="Path to the configuration file")
 
 args, beam_args = parser.parse_known_args()
